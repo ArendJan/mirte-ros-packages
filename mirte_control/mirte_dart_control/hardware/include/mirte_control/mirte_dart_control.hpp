7b31fdfe0dcc4744e10443e6b27cf3e9c2280a7b
S-vandergoot <siem.vandergoot@outlook.com>
2026-05-12 13:59:45 +0000

    Bugfixes

---
+++ mirte_control/mirte_dart_control/hardware/include/mirte_control/mirte_dart_control.hpp
@@ -0,0 +1,120 @@
+// Copyright 2021 ros2_control Development Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#error USING THE CORRECT HEADER
+
+#ifndef MIRTE_DART_CONTROL__HARDWARE_INTERFACE_HPP_
+#define MIRTE_DART_CONTROL__HARDWARE_INTERFACE_HPP_
+
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+#include <mirte_msgs/srv/set_servo_angle.hpp>
+#include <optional>
+
+#include "hardware_interface/handle.hpp"
+#include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/clock.hpp"
+#include "rclcpp/duration.hpp"
+#include "rclcpp/macros.hpp"
+#include "rclcpp/time.hpp"
+#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
+#include "rclcpp_lifecycle/state.hpp"
+
+
+namespace mirte_dart_control
+{
+struct JointValue
+{
+  double position{0.0};
+  double velocity{0.0};
+  double effort{0.0};
+};
+
+struct Joint
+{
+  explicit Joint(const std::string & name) : joint_name(name)
+  {
+    state = JointValue();
+    command = JointValue();
+  }
+
+  Joint() = default;
+
+  std::string joint_name;
+  JointValue state;
+  JointValue command;
+};
+class MirteDartHWInterface : public hardware_interface::SystemInterface
+{
+public:
+  RCLCPP_SHARED_PTR_DEFINITIONS(MirteDartHWInterface)
+
+  hardware_interface::CallbackReturn on_init(
+    const hardware_interface::HardwareInfo & info) override;
+
+  // Newly added for Humble
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+  // ----------------------
+
+  hardware_interface::CallbackReturn on_configure(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  hardware_interface::CallbackReturn on_activate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  hardware_interface::CallbackReturn on_deactivate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  hardware_interface::return_type read(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+  hardware_interface::return_type write(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+private:
+  std::optional<rclcpp::Logger> logger_;
+
+  // Service clients
+  rclcpp::Client<mirte_msgs::srv::SetServoAngle>::SharedPtr steering_client_;
+  rclcpp::Client<mirte_msgs::srv::SetServoAngle>::SharedPtr throttle_client_;
+
+  // Store last sent commands to avoid flooding the bus
+  int last_cmd_steering_;
+  int last_cmd_throttle_;
+  // Parameters for the CarlikeBot simulation
+  double hw_start_sec_;
+  double hw_stop_sec_;
+
+  // joint names
+  std::string steering_joint_;
+  std::string traction_joint_;
+
+  //Newly added for Humble
+  double steering_pos_cmd_ = 0.0;
+  double steering_pos_state_ = 0.0;
+  double traction_vel_cmd_ = 0.0;
+  double traction_vel_state_ = 0.0;
+  double traction_pos_state_ = 0.0;
+  //Newly added for Humble
+};
+
+}  // namespace mirte_dart_control
+
+#endif  // MIRTE_DART_CONTROL__HARDWARE_INTERFACE_HPP_
